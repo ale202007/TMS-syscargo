9493c20d54925232b1fe1442d04818a79dd74127
eviluster <eviluster87@gmail.com>
2025-03-31 15:23:49 -0400

    update src

--- src/components/AboutBlock.vue
+++ src/components/AboutBlock.vue
@@ -138,6 +138,6 @@
   left: -4rem;
   width: 108px;
   height: 93px;
-  background: url('/shape.png') no-repeat center/cover;
+  background: url('/src/assets/shape.png') no-repeat center/cover;
 }
 </style>
