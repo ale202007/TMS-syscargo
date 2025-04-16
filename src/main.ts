86b9dfe0869054f31750f1b87a3bd097f2fc3fa4
eviluster <eviluster87@gmail.com>
2025-04-16 18:21:29 -0400

    update proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,40 @@
-import './assets/main.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { Tooltip } from "bootstrap";
+import App from "./App.vue";
 
-// Vuetify
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/dist/vuetify.min.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { aliases, fa } from 'vuetify/iconsets/fa'
-import { mdi } from 'vuetify/iconsets/mdi'
-import App from './App.vue'
-import router from './router'
+/*
+TIP: To get started with clean router change path to @/router/clean.ts.
+ */
+import router from "./router";
+import ElementPlus from "element-plus";
+import i18n from "@/core/plugins/i18n";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    aliases,
-    sets: {
-      mdi,
-      fa,
-    },
-  },
-})
-const app = createApp(App)
+//imports for app initialization
+import ApiService from "@/core/services/ApiService";
+import { initApexCharts } from "@/core/plugins/apexcharts";
+import { initInlineSvg } from "@/core/plugins/inline-svg";
+import { initVeeValidate } from "@/core/plugins/vee-validate";
+import { initKtIcon } from "@/core/plugins/keenthemes";
 
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
-app.mount('#app')
+import "@/core/plugins/prismjs";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus);
+
+ApiService.init(app);
+initApexCharts(app);
+initInlineSvg(app);
+initKtIcon(app);
+initVeeValidate();
+
+app.use(i18n);
+
+app.directive("tooltip", (el) => {
+  new Tooltip(el);
+});
+
+app.mount("#app");
--- src/views/Contactenos.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import Contactenos from '@/components/Contactenos.vue'
-</script>
-
-<template>
-  <Contactenos />
-</template>
